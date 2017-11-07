b286d9319ccdd02d88b6c54d7658bf09c72ab13f
TJ Loughry <tjloughry@gmail.com>
2017-11-07 12:21:30 -0600

    random page added, pulls first from api list, no random functionality yet

--- src/components/results/Random.js
+++ src/components/results/Random.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Random extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      location: '',
+      randomPick: ''
+    }
+
+    this.handleRandom=this.handleRandom.bind(this);
+  }
+
+  handleRandom(e){
+    console.log(this.refs.locationseed.value);
+    axios.get('https://yelpprox.herokuapp.com/search?location=' + this.refs.locationseed.value)
+    .then((res) => {
+      this.setState({
+        randomPick: res.data.businesses[0].name
+      })
+    })
+    .catch((error)=>{
+      console.log('error');
+    })
+
+    // this.setState({
+    //   randomPick: this.refs.locationseed.value
+    // })
+    //set location state
+    //do axios call to Yelp API
+    //change state to display random pick
+
+  }
+
   render() {
     return (
-      <h1>Dont even care?</h1>
+      <div className='random-container'>
+        <h1>Dont even care?</h1>
+        <input type='text' placeholder='City and State OR Zipcode' ref='locationseed'/>
+        <input type='button' value='Roll The Dice' onClick={this.handleRandom}/>
+        <div className='ramdomOutputContainer'>
+          {this.state.randomPick}
+        </div>
+      </div>
     )
   }
 }

15b625e5619e1208397e5582ea75e682acb0d810
Jay Lara <jay.lara@outlook.com>
2017-11-09 19:15:03 -0600

    cleaned up code a bit more. added more comments. removed unneeded imports, etc

--- src/components/results/Random.js
+++ src/components/results/Random.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import { BrowserRouter as Router, Route, Link, NavLink, Switch } from 'react-router-dom';
-import Header from '../common/Header';
-import Search from '../results/Search';
-// import './Random.css';
-
-
 import './Random.css'
 
 //Displays a random location, from an axios call to Yelp API
@@ -14,7 +8,6 @@ export default class Random extends Component {
   constructor(props) {
     super(props);
     this.state={
-      location: '',
       randomPick: '',
       location :{latitude: null,longitude: null}
     } // end of initialize this.state
@@ -24,31 +17,42 @@ export default class Random extends Component {
     this.formatAddress=this.formatAddress.bind(this);
     this.formatCategories=this.formatCategories.bind(this);
     this.formatPhoneNumber=this.formatPhoneNumber.bind(this);
-
   } // end of constructor
 
+
+  //handles a click event. toggles isMapHidden flag each time map icon is clicked.
+  toggleMap(e) {
+    this.setState({
+      isMapHidden : !this.state.isMapHidden
+    })
+  } // end of toggleMap()
+
+  //handles a click event. uses value of locationseed textbox to populate ajax request string
   handleRandom(e){
     e.preventDefault();
+    //exits if locationseed is empty
+    if(!this.refs.locationseed.value) {
+      if (navigator.geolocation) { //geolocation is available
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.setState({
+            location : position.coords
+          })
+        });
+      } //end of navigator.geolocation check
+    }// end of if
 
-    let idLength = 50;
-    axios.get(`https://yelpprox.herokuapp.com/search?term=restaurant&limit=${idLength}&location=${this.refs.locationseed.value}`)
+    axios.get(`https://yelpprox.herokuapp.com/search?term=restaurant&limit=50&location=${this.refs.locationseed.value}`)
     .then((res) => {
-      //check each business' categories
-      //remove businesses that
-      idLength = res.data.businesses.length
-
-      let randomIndex = Math.floor(Math.random()*idLength)
       this.setState({
-        randomPick: res.data.businesses[randomIndex]
+        randomPick: res.data.businesses[Math.floor(Math.random()*res.data.businesses.length)]
       })
     }) // end of axios.get.then
     .catch((error)=>{
-      console.log('error');
       this.setState({
         randomPick: null,
         isMapHidden: true
       })
-    }) // end of axios.get.then
+    }) // end of axios.get.catch
   } // end of handleRandom()
 
   //Returns a string representation of a business' formatted address
@@ -60,7 +64,7 @@ export default class Random extends Component {
     address += location.city ? ", "+location.city : "";
     address += location.state ? ", "+location.state : "";
     address += location.zip_code ? " "+location.zip_code : "";
-    return address
+    return address;
   } // end of formatAddress()
 
   //Returns a string representation of a business' categories
@@ -88,27 +92,6 @@ export default class Random extends Component {
     } // end of else
   } // end of formatPhoneNumber()
 
-  toggleMap(e) {
-    this.setState({
-      isMapHidden : !this.state.isMapHidden
-    })
-  }
-
-  componentDidMount() {
-    if ("geolocation" in navigator) { //geolocation is available
-
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position.coords.latitude, position.coords.longitude);
-        this.setState({
-          location : position.coords
-        })
-      });
-    } else { //geolocation IS NOT available
-      console.log ("error");
-
-    }
-  }
-
   render() {
     var business = this.state.randomPick;
 
@@ -117,11 +100,12 @@ export default class Random extends Component {
     var categories = "";
     var phonenum = "";
     var staticMapSrc = "";
-    var staticMapStyle = "";
     var randomOutputContainer = {};
+
+    //if a business found, format business output fields
     if (business) {
       location = (business.location) ? this.formatAddress(business.location): '';
-      isOpened = business.is_closed ? <span>Closed</span> : <span>Opened</span>;
+      isOpened = (business.is_closed) ? <span>Closed</span> : <span>Opened</span>;
       categories = this.formatCategories(business);
 
       phonenum = (business.phone)?this.formatPhoneNumber(business.phone):'';
@@ -129,12 +113,12 @@ export default class Random extends Component {
         backgroundImage: `url(${business.image_url})`,
         visibility:'visible'
       };
-      var staticMapSrc=`https://maps.googleapis.com/maps/api/staticmap?`
+      //Google Static Map API call
+      staticMapSrc=`https://maps.googleapis.com/maps/api/staticmap?`
                         //+ `center=${this.state.location.latitude},${this.state.location.longitude}`
                         + `center=${business.coordinates.latitude},${business.coordinates.longitude}`
                         + `&zoom=13&size=400x400&sensor=false`
                         + `&key=AIzaSyDDQfQVi8sWBRMBLHMfmkcBko6r44-mo9o`;
-      this.formatCategories(business);
     } // end of if
 
     return (
@@ -142,19 +126,23 @@ export default class Random extends Component {
         <h1>Dont even care?</h1>
         <input type='text' placeholder='City and State OR Zipcode' ref='locationseed'
                 onKeyDown={(e) => {if(e.keyCode === 13) this.handleRandom(e)}}/>
-        <input type='button' value='Roll The Dice' onClick={this.handleRandom}/>
+        <input type='button' value='Roll The Dice' className="roll-dice" onClick={this.handleRandom}/>
+        <div className="roll-dice"></div>
 
         <div className="random-container-output" style={randomOutputContainer}>
 
           <div className="business-title">
-            <a href={business.url} target="_blank" title={business.name}><h1>{business.name}</h1></a>
+            <a href={business.url} target="_blank" title={business.name}>
+              <h1>{business.name}</h1>
+            </a>
+            <h1 onClick={this.toggleMap}>🗺</h1>
+
             <div>
               <h3>{business.price}</h3>
               <h3>{business.rating}</h3>
             </div>
           </div>
-          <div className="business-map" onClick={this.toggleMap}>
-            <div>🗺</div>
+          <div className="business-map" >
             {this.state.isMapHidden && <img id="static-map" src={staticMapSrc} alt={business.name} /> }
           </div>
           <div className="business-title">
@@ -162,7 +150,6 @@ export default class Random extends Component {
             <p>{location}</p>
             <p>{phonenum}</p>
             <p>{isOpened}</p>
-
           </div>
         </div>
       </div>

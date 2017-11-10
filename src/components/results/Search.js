bded62da1605646d5ad5719ea4175b936476f1c5
CescoIV <crordaz@gmail.com>
2017-11-09 18:34:46 -0600

    cleaned Search.js code, added comments, removed unnecessary methods/ variables

--- src/components/results/Search.js
+++ src/components/results/Search.js
@@ -13,33 +13,21 @@ export default class Search extends Component {
       hasSearched: false,
       inputs: [],
       data:[],
-      myCity: null
+      myCity: null,
+      toggle: null
     };
-
-    this.access = {
-      client_id: '3-N_879q6D3M6P3s3KplCA',
-      client_secret: 'KqyZ9kAqWo6VUCyRfUau0fWCTyJpPpicNj2e7y69emmTosk4yqJHGrcyDX1UZ5vW',
-      access_token: "zhu9COybkCTwEVuw-DQGBnJPx0mUdbtEEG9H-_5ouY6m-yZkcVxQjzkv1D0jXO0gkcSlq6_SS5okYRpJo9ZTy2dBPBW1uR_Ffm6YstSPMvsddTXUtzc66DQnhoIAWnYx",
-      expires_in: 637498157,
-      token_type: "Bearer"
-    }
-
-    this.stuff= null;
-
-    this.prepData = this.prepData.bind(this);
     this.prepareQuery = this.prepareQuery.bind(this);
     this.axiosRequest = this.axiosRequest.bind(this);
     this.updateArray = this.updateArray.bind(this);
     this.makeMyObject = this.makeMyObject.bind(this);
   }
-
   handleChange(i, event) {
      let value = this.state.value.slice();
      value[i] = event.target.value;
      this.setState({value});
   }
-
   addClick(){
+    //adds or limits how many fields you can have
     if (this.state.count < 3) {
       this.setState({count: this.state.count+1})
     }
@@ -47,8 +35,8 @@ export default class Search extends Component {
       alert('You can only have up to three fields.')
     }
   }
-
   removeClick(i){
+    //remvoes input fields
      let value = this.state.value.slice();
      value.splice(i,1);
      if (this.state.count > 1) {
@@ -61,8 +49,8 @@ export default class Search extends Component {
       alert("You must have at least one search field.");
     }
   }
-
   createUI(){
+    //makes the actual input components
      let inputFields = [];
      for(let i = 0; i < this.state.count; i++){
        inputFields.push(
@@ -72,50 +60,52 @@ export default class Search extends Component {
         )
      }
      return inputFields || null;
+  }  
+  updateArray(e) {
+    //called onBlur of an input field, adds that value to our inputs array
+    this.state.inputs.push(e.target.value);
   }
-
-  axiosRequest(foodTerm) {
-    let limit = 3;
-    let url = `https://yelpprox.herokuapp.com/search?term=${ foodTerm }&limit=${ limit }&location=${ this.state.myCity }`;
-
-    axios.get(url)
-    .then((response) => {
-      this.prepData(response);
-    }).catch(e => e);
-  }
-
   prepareQuery(e) {
+    //for each input field, we keep track of them on the state, this method sends an axios request for each of those items
     e.preventDefault();
 
     for (let i of this.state.inputs) {
       this.axiosRequest(i);
     }
-
+    //after the search, set the data and inputs to blank, so the user can search again without stacking the searches
     this.setState({
       inputs: [],
       data: []
     })
   }
+  axiosRequest(foodTerm) {
+    let limit = 3; //set search limit to 3 for yelp API endpoint
+    let url = `https://yelpprox.herokuapp.com/search?term=${ foodTerm }&limit=${ limit }&location=${ this.state.myCity }`;
 
-  updateArray(e) {
-    this.state.inputs.push(e.target.value);
+    axios.get(url)
+    .then((response) => {
+      this.makeMyObject(response.data.businesses); //sends data to makeMyObj, which saves the data to our state
+    }).catch(e => e);
   }
-
-  prepData(data) {
-    this.makeMyObject(data.data.businesses);
-  }
-
   makeMyObject(businesses) {
-
+    //for each business data object found, save it to our state
+    //and change hasSearched to true (toggles the view)
     for(let i of businesses){
       this.setState({
         hasSearched: true,
         data: this.state.data.concat([i])
       })
     }
+    this.setState({
+      toggle: (
+      <div className="render-results">
+        {this.makeMyChild()}
+      </div>
+      ) 
+    })
   }
-
   makeMyChild(){
+    //goes through saved API data and creates appendable HTML elements
     let resultDivs = [];
     for(let i = 0; i < this.state.data.length ; i++) {
       resultDivs.push(
@@ -130,66 +120,57 @@ export default class Search extends Component {
         </div>
       )
     }
-
+    //shuffles the array of all data, returns only the first 3 to provide a more random/dice like experience
     let rand = this.shuffle(resultDivs);
     resultDivs = rand.slice(0,3);
-
     return resultDivs || null;
-}
-
-shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+  }
+  shuffle(arr) {
+    //shuffles arr in place
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-
-    return a;
-}
-
+    return arr;
+  }
 
   render() {
     let display;
     let resultsDisplay;
-      display = (
-        <div className="input-area">
-        <div className='searchTypeLinks test9'>
-          <ul className='searchTypeLinksList'>
-            <li className='listItem' id='selected'><NavLink className='links searchLink' to='/Search'>Search</NavLink></li>
-            <li className='listItem'><NavLink className='links randomLink' to='/Random'>Don't Even Care!</NavLink></li>
-          </ul>
-        </div>
-          <h1 className="header">What are you craving?</h1>
-          <div className="form-container">
-            <div id="another-div">
-              <input className="zip-code" placeholder='City or Zip' onBlur={(e) => this.setState({ myCity: e.target.value })} />
-              <div className="forms">
-                <div id="food-inputs">
-                  {this.createUI()}
-                </div>
+    display = (
+      <div className="input-area">
+      <div className='searchTypeLinks test9'>
+        <ul className='searchTypeLinksList'>
+          <li className='listItem' id='selected'><NavLink className='links searchLink' to='/Search'>Search</NavLink></li>
+          <li className='listItem'><NavLink className='links randomLink' to='/Random'>Don't Even Care!</NavLink></li>
+        </ul>
+      </div>
+        <h1 className="header">What are you craving?</h1>
+        <div className="form-container">
+          <div id="another-div">
+            <input className="zip-code" placeholder='City or Zip' onBlur={(e) => this.setState({ myCity: e.target.value })} />
+            <div className="forms">
+              <div id="food-inputs">
+                {this.createUI()}
               </div>
             </div>
-            <form onSubmit={ this.prepareQuery }>
-              <div id='add-remove-buttons'>
-                <input id='add-input' type='button' value='+' onClick={this.addClick.bind(this)}/>
-                <input id='remove-input' type='button' value='-' onClick={this.removeClick.bind(this)} />
-              </div>
-              <input id='submit-cravings' type="submit" value="Submit" />
-            </form>
           </div>
+          <form onSubmit={ this.prepareQuery }>
+            <div id='add-remove-buttons'>
+              <input id='add-input' type='button' value='+' onClick={this.addClick.bind(this)}/>
+              <input id='remove-input' type='button' value='-' onClick={this.removeClick.bind(this)} />
+            </div>
+            <input id='submit-cravings' type="submit" value="Submit" />
+          </form>
         </div>
-        
-        )
+      </div>
+      
+      )
 
-       resultsDisplay = (
-        <div className="render-results">
-          {this.makeMyChild()}
-        </div>
-        )
-
+    this.toggle = this.state.hasSearched ? resultsDisplay : display; 
     return (
       <div>
-        {display}
-        {resultsDisplay}
+        {this.toggle}
       </div>
     );
   }
